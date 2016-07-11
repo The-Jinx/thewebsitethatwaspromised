1cdc7d013447dddd5214420358ce22c5ef63ed37
Andrew <andyukr@i.ua>
2016-07-11 21:27:17 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-$(document).ready(function(){
-    $("#books").click(function(){
-        $("section").toggle();
-    });
+$("#books").click(function(){
+        $("#books_section").show();
+        $("#main").hide();
+        $("#interests_section").hide();
+});
+$("#interests").click(function(){
+        $("#interests_section").show();
+        $("#main").hide();
+        $("#books_section").hide();
 });
 
